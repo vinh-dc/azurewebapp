460f97472f9b11cc928e1d28b632d055b8db8262
Vinh Chung <Vinh.Chung@datacom.com.au>
2022-02-17 10:55:25 +1100

    use express

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const { createServer } = require("https");
-const { parse } = require("url");
-const next = require("next");
-const fs = require("fs");
-const port = 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const express = require('express')
+const next = require('next')
 
-const httpsOptions = {
-    key: fs.readFileSync("./https_cert/localhost-key.pem"),
-    cert: fs.readFileSync("./https_cert/localhost.pem")
-};
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-    createServer(httpsOptions, (req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl);
-    }).listen(port, (err) => {
-        if (err) throw err;
-        console.log("ready - started server on url: https://localhost:" + port);
-    });
-});
+// Your app will get the Azure port from the process.enc.PORT
+const port = process.env.PORT || 3000;
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, err => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
+  })
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
