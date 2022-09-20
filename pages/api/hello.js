02a4e399199b13736dcd5c074073062f23ce3363
Vinh Chung <Vinh.Chung@datacom.com.au>
2022-09-21 09:23:40 +1000

    test env var

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe', key: process.env.API_KEY })
+  res.status(200).json({ name: 'Denny Doe', key: process.env.API_KEY })
 }
